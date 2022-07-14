85f1e8deb62ad6fef202b43cd8edabb4c10dcdba
IlgnerSilva <ilgnersilva@outlook.com>
2022-07-14 11:36:50 -0300

    Adicionando link para currículo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,10 +7,10 @@ interface Props {
     user: string | null | undefined
 }
 
-export default function Header ({setUser, user}: Props){
-    function logout(){
+export default function Header({ setUser, user }: Props) {
+    function logout() {
         auth.signOut();
-        auth.signOut().then(val =>{
+        auth.signOut().then(val => {
             setUser(null);
         });
     }
@@ -25,14 +25,17 @@ export default function Header ({setUser, user}: Props){
                         <p>
                             Olá <strong>@{user}</strong>
                         </p>
-                        <Button onClick={()=> logout()} value='Sair'/>
+                        <Button onClick={() => logout()} value='Sair' />
                     </div>
                 ) : (
                     <div>
-                        <Form setUser={setUser}/>
+                        <Form setUser={setUser} />
                     </div>
                 )}
+                <div>
+                    <a target="blank" className='lock w-full bg-violet-900 mt-5 p-2 rounded-2xl hover:bg-violet-800 hover:-translate-y-1 transition-all duration-500 text-white font-semibold mb-2' href="https://docs.google.com/document/d/1-xR64v1HS56s_SAgoFWGVyjLIYTyOaJPyxISjtOtnKs/edit?usp=sharing">Ver Currículo</a>
                 </div>
+            </div>
         </header>
     )
 }
